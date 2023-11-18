06664350663ed789c9a2a4031f4cb06296f66645
Dmitry Sokolov <dmitry.k.sokolov@gmail.com>
2023-11-18 20:02:05 +0300

    Svelte, Tailwind and plugins configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,23 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
+	extends: ["eslint:recommended", "plugin:svelte/recommended", "prettier"],
+	plugins: ["unused-imports", "simple-import-sort", "import"],
+	rules: {
+		"simple-import-sort/imports": "error",
+		"simple-import-sort/exports": "error",
+		"import/first": "error",
+		"import/newline-after-import": "error",
+		"import/no-duplicates": "error",
+		"unused-imports/no-unused-imports": "error",
+		"unused-imports/no-unused-vars": [
+			"warn",
+			{ vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" }
+		]
+	},
 	parserOptions: {
-		sourceType: 'module',
+		sourceType: "module",
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: [".svelte"]
 	},
 	env: {
 		browser: true,
